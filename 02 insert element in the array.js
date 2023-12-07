7c413917def5b84f72345558f736cc3ca7e1bf0c
Abdul Wahab Shaikh <aw599822@gmail.com>
2023-12-07 20:26:59 +0500

    02 file

---
+++ 02 insert element in the array.js
@@ -0,0 +1,11 @@
+let array =[1,2,3,5,6];
+let insertelement= 555;
+let position = 4;
+
+array = array.filter((element)=>
+
+{
+    return array[element+1] = array[element];
+
+})
+console.log(array);
